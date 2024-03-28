22f96cc0bb22d195cc5ad602e40f7e76d4f875af
AbdullahFaisal63 <f219294@cfd.nu.edu.pk>
2024-03-28 17:52:03 +0500

    configured mysql using sequelize

---
+++ server/models/users.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const users = sequelize.define("users", {
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    })
+
+    return users
+}
